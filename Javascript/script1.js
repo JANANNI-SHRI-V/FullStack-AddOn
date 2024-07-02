0023c499523ce438997d58158daf032bc97eb405
KGCAS STUDENTS <casstudent@kgisledu.com>
2024-07-02 10:52:05 +0530

    first commit

---
+++ Javascript/script1.js
@@ -0,0 +1,45 @@
+// variables
+a=10;
+b=10;
+console.log(a+b);
+
+name="Jananni";
+greet="Vanakkam";
+console.log(greet,name);
+{
+    var name='shri';
+}
+let greeting='ena savukiyam ahh';
+const space=' ';
+greeting='hiiiii guysss';
+console.log(name+space+greeting);
+
+{
+    var name='nanbargalehh';
+    let greets='makkalehh ipo ena aachu na ';
+    const space=' ';
+    console.log(name+space+greeting);
+}
+// Operators
+// arithmetic operator
+let j=9;
+let s=9;
+console.log(j+s);
+console.log(j-s);
+console.log(j*s);
+console.log(j/s);
+console.log(j%s);
+console.log(j**s);
+// unary operator
+let y=1;
+let z=2;
+console.log(++y);
+console.log(y++);
+console.log(y);
+console.log(--z);
+console.log(z--);
+console.log(z);
+console.log(z++ + y + z++ + z++ +y);
+console.log(y);
+console.log(z);
+
